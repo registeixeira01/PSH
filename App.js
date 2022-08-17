8079b4d207725a5b9b8187bf241bccd9864f3fe0
Régis Teixeira <regismarcondes2016@gmail.com>
2022-08-16 23:12:24 -0300

    Iniciando Projeto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title} >PSH</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#1E90FF',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fffff',
   },
 });
